8aaabee9eaba3e33ac53cc728f02ce60807d9d63
ArtyoZ <43533126+ArtyoZ@users.noreply.github.com>
2026-09-21 14:48:46 +0300

    Create AssemblyInfo.cs

---
+++ properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+/*
+    KeeUnicodeURL - KeePass plugin
+    AssemblyInfo.cs
+*/
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KeeUnicodeURL")]
+[assembly: AssemblyDescription("Adds an editable Unicode URL field to the KeePass entry editor and a Unicode URL column to the entry list for IDN/Punycode (xn--) domains.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Artyom Zhurkin")]
+[assembly: AssemblyProduct("KeePass Plugin")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
